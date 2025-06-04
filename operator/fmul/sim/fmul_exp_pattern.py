e60b6e43d3f82a5f5e616a9a6dfe880e4cd6352c
hsuan-0701 <rock123871@gmail.com>
2025-06-04 19:46:49 +0800

    Add files via upload

---
+++ operator/fmul/sim/fmul_exp_pattern.py
@@ -0,0 +1,31 @@
+import random
+
+pattern_num = 50000
+
+#############################################################################################
+#                                  EXP CALCULATE                                            #
+#############################################################################################
+def exp_calculate (ea ,eb):
+    ea_real = ea -1023
+    eb_real = eb -1023
+    e = ea_real + eb_real
+    e = e+1023
+    # if(ea == 2047 or eb == 2047): e =2047
+    # if(C==1):   e=0
+    return e
+
+with open("exp_A.dat", "w") as f_a, open("exp_B.dat", "w") as f_b, open("exp_golden.dat", "w") as f_g , open("out_inf.dat","w") as f_c:
+    for i in range(pattern_num):
+        A = random.randint(0, 2047)  # 11-bit A
+        B = random.randint(0, 2047)  # 11-bit B
+        golden = exp_calculate(A,B)   # 11-bit exp 結果
+        out_inf = 0
+        if A==2047 : out_inf = 1
+        if B==2047 : out_inf = 1
+        # 寫入各自檔案（十進位格式）
+        f_a.write(f"{A}\n")
+        f_b.write(f"{B}\n")
+        f_c.write(f"{out_inf}\n")
+        f_g.write(f"{golden}\n")
+
+print("成功產出 A.dat, B.dat, GOLDEN.dat 各 5000 筆資料")
