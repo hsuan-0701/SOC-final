860a9f8d3e71859f9496f04738b291ce64375e91
hsuan-0701 <rock123871@gmail.com>
2025-06-14 01:06:16 +0800

    Add files via upload

--- operator/fp_add/hdl/fp_add.v
+++ operator/fp_add/hdl/fp_add.v
@@ -17,7 +17,8 @@
 // Date           by            Version       Change Description
 // 2025.5.26    hsuanjung,Lo      2.0         fix rounding bug
 // 2025.5.26    hsuanjung,Lo      3.0         fix exponent bug in first normalization
-// 2025.6.1     hsuanjung,lo      4.0         change the included module name "LOD" => "LOD_128" 
+// 2025.6.1     hsuanjung,lo      4.0         change the included module name "LOD" => "LOD_128"
+// 2025.6.13    hsuanjung,lo      5.0         solve inf input case 
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //  `include "add_107.v"
@@ -25,6 +26,8 @@
 //  `include "CLA_8.v"
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //==================================================================================================================================================================================
+//
+// * Don't consider NaN case , this case should solve in upper module.( input contain NaN or both pos、neg inf )
 //
 //  IEEE 754 double precision floating point form(64bit width)
 //     1bit     11bit       52bit
@@ -165,6 +168,13 @@ reg                                 pip5_v;
 /////////////////////////////////////////////////////////////////////////////////////////
 //                                  Decode                                             //
 /////////////////////////////////////////////////////////////////////////////////////////
+wire inf_a;
+wire inf_b;
+wire NaN  ;
+wire mantissa_nonzero_a;
+wire mantissa_nonzero_b;
+
+assign NaN    = (in_A[pDATA_WIDTH-1] != in_B[pDATA_WIDTH-1])? (inf_a & inf_b) : ((inf_A & mantissa_nonzero_a) | (inf_B & mantissa_nonzero_b));
 
 assign exp_a  = in_A[(pDATA_WIDTH-2) : pFRAC_WIDTH]; 
 assign exp_b  = in_B[(pDATA_WIDTH-2) : pFRAC_WIDTH];
@@ -172,9 +182,14 @@ assign exp_b  = in_B[(pDATA_WIDTH-2) : pFRAC_WIDTH];
 assign hid_a  = |exp_a;
 assign hid_b  = |exp_b;
 
+assign inf_a  = &(exp_a);
+assign inf_b  = &(exp_b);
 
 assign frac_a = {hid_a , in_A[(pFRAC_WIDTH-1) : 0]};    //*  expand hidden bit of fraction
 assign frac_b = {hid_b , in_B[(pFRAC_WIDTH-1) : 0]};    //*  expand hidden bit of fraction
+
+assign mantissa_nonzero_a  = (| (in_A[(pFRAC_WIDTH-1):0]));
+assign mantissa_nonzero_b  = (| (in_B[(pFRAC_WIDTH-1):0]));
 
 assign sign_a = in_A[pDATA_WIDTH-1];
 assign sign_b = in_B[pDATA_WIDTH-1];
@@ -190,6 +205,9 @@ assign exp_diff_ba = exp_b - exp_a ;
 ///////////////////////////////////////////////////////////////////////////////////////
 //                              PIPELINE stage1                                      //
 ///////////////////////////////////////////////////////////////////////////////////////
+reg pip1_inf_a ;
+reg pip1_inf_b ;
+reg pip1_NaN   ;
 
 always @(posedge clk or negedge rst_n) begin
     if(!rst_n)begin
@@ -197,6 +215,9 @@ always @(posedge clk or negedge rst_n) begin
         pip1_exp_diff_ab <= {(pEXP_WIDTH){1'b0}}; 
         pip1_exp_diff_ba <= {(pEXP_WIDTH){1'b0}}; 
         
+        pip1_NaN         <= 1'b0;
+        pip1_inf_a       <= 1'b0 ;
+        pip1_inf_b       <= 1'b0 ;
         pip1_frac_a      <= {(pFRAC_WIDTH+1){1'b0}} ;
         pip1_frac_b      <= {(pFRAC_WIDTH+1){1'b0}} ;
         pip1_sign_a      <= 1'b0;
@@ -209,6 +230,9 @@ always @(posedge clk or negedge rst_n) begin
         pip1_exp_diff_ab <= exp_diff_ab;
         pip1_exp_diff_ba <= exp_diff_ba;
 
+        pip1_NaN         <= NaN ;
+        pip1_inf_a       <= inf_a ;
+        pip1_inf_b       <= inf_b ;
         pip1_frac_a      <= frac_a ;
         pip1_frac_b      <= frac_b ;
         pip1_sign_a      <= sign_a;
@@ -266,11 +290,17 @@ end
 /////////////////////////////////////////////////////////////////////////////////////////////
 //                              PIPELINE stage2                                           //
 /////////////////////////////////////////////////////////////////////////////////////////////
+reg pip2_inf_a ;
+reg pip2_inf_b ;
+reg pip2_NaN   ;
 
 always @(posedge clk or negedge rst_n)begin
     if(!rst_n)begin
         pip2_op           <= 2'd0;
 
+        pip2_NaN          <= 1'b0 ;
+        pip2_inf_a        <= 1'b0 ;
+        pip2_inf_b        <= 1'b0 ;
         pip2_sign_ab      <= 2'd0;
 
         pip2_frac_a       <= {(pADDER_WIDTH){1'b0}};
@@ -282,6 +312,9 @@ always @(posedge clk or negedge rst_n)begin
         pip2_op           <= op;
         pip2_sign_ab      <= sign_ab;
 
+        pip2_NaN          <= pip1_NaN   ;
+        pip2_inf_a        <= pip1_inf_a ;
+        pip2_inf_b        <= pip1_inf_b ;
         pip2_frac_a       <= frac_a_shifted ;
         pip2_frac_b       <= frac_b_shifted ;
 
@@ -330,14 +363,26 @@ end
 ////////////////////////////////////////////////////////////////////////////////
 //                            Pipeline stage 3                                //
 ////////////////////////////////////////////////////////////////////////////////
+reg pip3_inf_a ;
+reg pip3_inf_b ;
+reg pip3_NaN   ;
+wire inf_case  ;
+
+
 
 always @(posedge clk or negedge rst_n) begin
     if(!rst_n)begin
+        pip3_NaN         <= 1'b0 ;
+        pip3_inf_a       <= 1'b0 ;
+        pip3_inf_b       <= 1'b0 ;
         pip3_frac_result <= {(pADDER_WIDTH){1'b0}};
         pip3_exp         <= {(pEXP_WIDTH){1'b0}};
         pip3_sign_ab     <= 2'd0;
         pip3_v           <= 1'b0;
     end else begin
+        pip3_NaN         <= pip2_NaN   ;
+        pip3_inf_a       <= pip2_inf_a ;
+        pip3_inf_b       <= pip2_inf_b ;
         pip3_frac_result <= frac_result;
         pip3_exp         <= pip2_exp;
         pip3_sign_ab     <= pip2_sign_ab;
@@ -374,7 +419,7 @@ localparam  negative     = 1'b1;
 //---------------------------------------------------------- normalization ----------------------------------------------------------------------------------------------------------------------//
 assign frac_sign     = pip3_frac_result[pADDER_WIDTH-1];                                                                        // * pip3_frac_result is signed we need to collect its sign bit to calculate
 assign frac_abs      = (frac_sign)?  (((~ pip3_frac_result) + {{(pADDER_WIDTH-1){1'b0}} , 1'b1})) : (pip3_frac_result );          // * pip3_frac_result is signed we need to transfer it to absolute value !
-
+assign inf_case     = pip3_inf_a | pip3_inf_b ;
 
 // *assign frac_normal_0 = (frac_abs[(pADDER_WIDTH-1):0] << shift_amount) >>1 ;                                                       // * fisrt time normalize
 // *assign exp_normal_0  = (shift_amount <= maximum_shift)? (pip3_exp - shift_amount+1) : {(pEXP_WIDTH){1'b0}};                       // * first time normalize .If all zero ,replace exp = 0000(denormal type).
@@ -402,20 +447,29 @@ assign round_bit     = frac_normal_0[(pADDER_WIDTH-pFRAC_WIDTH-5)];
 assign sticky_bit    = | frac_normal_0[(pADDER_WIDTH-pFRAC_WIDTH-6) : 0];
 
 always @(*) begin
-    case(pip3_sign_ab)
-        posA_negB : result_sign = (frac_sign)?  negative : postive;
-        posA_posB : result_sign = postive ;
-        negA_negB : result_sign = negative;
-        negA_posB : result_sign = (frac_sign)?  negative : postive;
-    endcase
+    if(pip3_inf_a || pip3_inf_b)begin
+        result_sign = (pip3_inf_a)?  pip3_sign_ab[1] : pip3_sign_ab[0] ;  // * bit [1] as sign of A , bit [0] as sign of B
+    end else begin
+        case(pip3_sign_ab)
+            posA_negB : result_sign = (frac_sign)?  negative : postive;
+            posA_posB : result_sign = postive ;
+            negA_negB : result_sign = negative;
+            negA_posB : result_sign = (frac_sign)?  negative : postive;
+        endcase
+    end
 end
+
 
 ////////////////////////////////////////////////////////////////////////////////
 //                            Pipeline stage4                                 //
 ////////////////////////////////////////////////////////////////////////////////
+reg pip4_inf;
+reg pip4_NaN;
 
 always @(posedge clk or negedge rst_n) begin
     if(!rst_n)begin
+        pip4_NaN         <= 1'b0;
+        pip4_inf         <= 1'b0;
         pip4_lsb         <= 1'b0;
         pip4_sticky      <= 1'b0;
         pip4_guard       <= 1'b0;
@@ -426,6 +480,8 @@ always @(posedge clk or negedge rst_n) begin
         pip4_result_sign <= 1'b0;
         pip4_v           <= 1'b0;
     end else begin
+        pip4_NaN         <= pip3_NaN;
+        pip4_inf         <= inf_case;
         pip4_lsb         <= lsb;
         pip4_sticky      <= sticky_bit;
         pip4_guard       <= guard_bit;
@@ -466,8 +522,8 @@ assign frac_normal_0_expand = {1'b0 , pip4_frac};
 assign frac_rounded         = (round_op == ROUNDING)?  (frac_normal_0_expand + {{(pFRAC_WIDTH){1'b0}} , 1'b1}) : frac_normal_0_expand ;
 
 
-assign frac_normal_1        = (frac_rounded[pFRAC_WIDTH])?  ( frac_rounded >> 1) : frac_rounded  ;
-assign exp_normal_1         = (frac_rounded[pFRAC_WIDTH])?  (pip4_exp + {{(pEXP_WIDTH-1){1'b0}} , 1'b1})  : pip4_exp;
+assign frac_normal_1        = (pip4_NaN)? {{(pFRAC_WIDTH){1'b0}} , 1'b1} : ((pip4_inf)?  {(pFRAC_WIDTH+1){1'b0}} : ( (frac_rounded[pFRAC_WIDTH])?  ( frac_rounded >> 1) : frac_rounded ) );
+assign exp_normal_1         = (pip4_NaN)? {(pEXP_WIDTH){1'b1}}           : ((pip4_inf)?  {(pEXP_WIDTH){1'b1}}    : ( (frac_rounded[pFRAC_WIDTH])?  (pip4_exp + {{(pEXP_WIDTH-1){1'b0}} , 1'b1})  : pip4_exp));
 
 always @(*) begin
     if(pip4_guard)begin
@@ -482,6 +538,7 @@ end
 ////////////////////////////////////////////////////////////////////////////////
 //                            Pipeline stage5                                 //
 ////////////////////////////////////////////////////////////////////////////////
+
 
 
 always @(posedge clk or negedge rst_n) begin
